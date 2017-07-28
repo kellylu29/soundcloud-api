cd1fd0682cee3a401845f6c0e50c895754423ae1
Kelly <kellylovelu@gmail.com>
2017-07-28 15:14:16 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,84 @@
+//function to return getElementById
+function id(str){
+  return document.getElementById(str)
+}
+
+var playBtn = id('play')
+var pauseBtn = id('pause')
+var nextBtn = id('next')
+var artist = id('artist')
+var songTitle = id('title')
+var description = id('description')
+var genre = id('genre')
+var date = id('date')
+var artImg = id('artImg')
+
+var tracks = []
+var currentTrack = 0
+
+
+SC.initialize({
+  client_id: 'fd4e76fc67798bfa742089ed619084a6'
+})
+
+SC.get('/users/57031159/tracks').then(function(response){
+
+  tracks = response 
+
+  artist.innerHTML = tracks[currentTrack].user.username
+  artist.href = tracks[currentTrack].user.permalink_url
+  title.innerHTML = tracks[currentTrack].title
+  title.href = tracks[currentTrack].permalink_url
+
+
+  description.innerHTML = tracks[currentTrack].description +"description"
+  genre.innerHTML = tracks[currentTrack].genre + "genre"
+  date.innerHTML = tracks[currentTrack].release_year +"year"
+  artImg.src = tracks[currentTrack].artwork_url
+
+
+}).then(function(){
+
+  playSong()
+
+})
+  
+function playSong(){
+  SC.stream('/users/8362534/tracks/' + tracks[currentTrack].id).then(function(player){
+    
+    playBtn.addEventListener('click', function(){
+    player.play()
+    })
+
+    pauseBtn.addEventListener('click', function(){
+    player.pause()
+    })
+
+    nextBtn.addEventListener('click', function(){
+    currentTrack++
+    
+    player.play()
+    })
+
+  })
+}
+
+
+function playNext() {  
+  SC.stream( '/tracks/' + songs[currentTrack].id ).then(function(player){
+    player.play()
+    player.on("finish",function(){      
+    currentTrack += 1
+    playNext()     
+    })
+  })
+}
+  
+  // player.on("finish",function(){      
+  //   currentSong ++;       
+  //   playNext(); 
+  //   })
+
+
+  
+  
